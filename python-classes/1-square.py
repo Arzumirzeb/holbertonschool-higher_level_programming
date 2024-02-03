4bb4633a60e6f4d758ffffbab3fdd033199074b3
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-03 11:05:29 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square():
+    """Square class"""
+    def __init__(self, size):
+        """Initializer for square class"""
+        self.__size = size
