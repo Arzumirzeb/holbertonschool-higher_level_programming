bbec8e64e96c4b915799d1d27b2da3fc8c6e7446
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-03 17:45:49 -0800

    6-max_integer.py error fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,37 +8,33 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_ordered_list(self):
-        ordered_list = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered_list), 4)
-
-    def test_ordered_list(self):
-        ordered_list = [1, 3, 2, 4]
-        self.assertEqual(max_integer(unordered_list), 4)
-
-    def test_empty_list(self):
-        empty_list = []
-        self.assertEqual(max_integer(empty_list), None)
-
-    def test_max_beggining(self):
-        test_list = [4, 3, 2, 1]
-        self.assertEqual(max_integer(test_list), 4)
-
-    def test_max_ending(self):
         test_list = [1, 2, 3, 4]
         self.assertEqual(max_integer(test_list), 4)
 
-    def test_all_negative(self):
-        test_list = [-4, -5, -7, -1]
+    def test_unordered_list(self):
+        test_list = [1, 2, 4, 3]
         self.assertEqual(max_integer(test_list), 4)
 
-    def test_one(self):
+    def test_empty_list(self):
+        test_list = []
+        self.assertEqual(max_integer(test_list), None)
+
+    def test_max_first(self):
+        test_list = [4, 3, 2, 1]
+        self.assertEqual(max_integer(test_list), 4)
+
+    def test_max_end(self):
+        test_list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(test_list), 4)
+
+    def test_max_middle(self):
+        test_list = [4, 3, 5, 2, 1]
+        self.assertEqual(max_integer(test_list), 5)
+
+    def test_max_all_negative(self):
+        test_list = [-4, -5, -7, -1]
+        self.assertEqual(max_integer(test_list), -1)
+
+    def test_one_max(self):
         test_list = [50]
         self.assertEqual(max_integer(test_list), 50)
-
-    def test_floats(self):
-        test_list = [4.12, 3.45, 2.76, 1.81]
-        self.assertEqual(max_integer(test_list), 4)
-
-    def test_strings(self):
-        test_list = ["bir", "iki", "yeddi", "doqquz"]
-        self.assertEqual(max_integer(test_list), 4)
