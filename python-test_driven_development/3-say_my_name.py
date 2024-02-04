00d0f524f27a59f638d8f7f13e350a4cd7fa0a37
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-04 09:03:41 -0800

    ok

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""say_my_name function"""
+
+
 def say_my_name(first_name, last_name=""):
+    """define say_my_name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
