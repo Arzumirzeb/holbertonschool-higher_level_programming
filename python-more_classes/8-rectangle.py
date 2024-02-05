2d6c6550d5d957b95ffd934ca0a968eba57e982b
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-05 10:38:04 -0800

    Compare rectangles

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+ 
     def bigger_or_equal(rect_1, rect_2):
         """Biggest rectangle"""
         if not isinstance(rect_1, Rectangle):
