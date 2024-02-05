a1eeff5cf58a80c7b3e39c6943b240f9f37a267a
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-05 11:13:12 -0800

    print symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
 
         for i in range(self.__height):
             for j in range(self.__width):
-                end += "#"
+                end += str(self.print_symbol)
             if i != self.__height - 1:
                 end += '\n'
         return end
