a39a6009eda2552f4d0867df5d63ebb522e5b44e
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-06 10:06:43 -0800

    integer_validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,7 +5,9 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """Rectangle inheritd BaseGeometry"""
     def __init__(self, width, height):
         """Define function"""
         self.integer_validator("width", width)
