e921a80dacc2afdbc937310dc44d70b0b15feac6
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-06 13:25:34 -0800

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Read file"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
+        f.close()
