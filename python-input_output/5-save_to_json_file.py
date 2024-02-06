4466093e74469c520c396fb683572c01a8493f0f
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-06 14:07:48 -0800

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Save Object to a file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function that writes an Object to a text file"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
+    f.close()
+
