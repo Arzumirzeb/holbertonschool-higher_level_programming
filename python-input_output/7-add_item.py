c62cc08c545d9667cb8141e28bb33787bfd83a7b
arzumirzeb <arzumirzeb03@gmail.com>
2024-02-07 03:55:13 -0800

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Load, add, save"""
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    loads = load_from_json_file("add_item.json")
+except Exception:
+    loads = []
+save_to_json_file(loads + sys.argv[1:], "add_item.json")
