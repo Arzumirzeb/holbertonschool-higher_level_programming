4f52da0efeb5bd320602c1b49b8086fe8d8dc524
Arzumirzeb <143116114+Arzumirzeb@users.noreply.github.com>
2024-02-08 01:06:49 +0400

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,10 +12,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         temp = [1]
-        for j in range(len(triangle[i - 1]) -1):
+        for j in range(len(triangle[i - 1]) - 1):
             temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         temp.append(1)
         triangle.append(temp)
     return triangle
-
-
