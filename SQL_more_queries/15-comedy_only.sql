d3f96978da7c49e077287f99c2bf8d9e7512e8f5
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-21 01:16:50 +0400

    hotfix

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title FROM tv_shows
+SELECT tv_shows.title FROM tv_genres
 INNER JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = "Comedy"
