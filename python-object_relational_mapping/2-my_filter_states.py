6d5b838556522e5f6fa7b7e60718f19b81d7c432
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 21:50:31 +0400

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,12 @@ import sys
 import sqlalchemy
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", 
+                         port=3306, 
+                         user=sys.argv[1], 
+                         passwd=sys.argv[2], 
                          db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states
-                WHERE BINARY name = {}".format(sys.argv[4]))
+                WHERE BINARY name = '{}'".format(sys.argv[4]))
     [print(state) for state in cur.fetchall()]
