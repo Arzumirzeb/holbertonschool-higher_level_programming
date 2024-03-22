43f4686588fb55d1e1d1caf752305b0a0eb250e5
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 22:20:42 +0400

    hotfix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """displays all values where name matches the argument."""
 
-import MySQLdb
-
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
