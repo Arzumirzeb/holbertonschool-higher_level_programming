717c838074f629f2173d70e21697a5375bf71135
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-23 00:13:08 +0400

    9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id).all():
         if "a" in state.name:
             print("{}: {}".format(state.id, state.name))
+    session.close()
