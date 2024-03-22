6faafcb331e11fa74ffc306c6321e10ea783a4d1
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 22:33:31 +0400

    hotfix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.id, cities.name, states.name \
             FROM cities \
             INNER JOIN states \
+            ON cities.state_id = states.id \
             ORDER BY cities.id")
     [print(state) for state in cur.fetchall()]
