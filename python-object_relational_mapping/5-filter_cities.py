90a844675088bf257b8ef3ba6b3ba2201ba324a1
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 23:18:08 +0400

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ import sqlalchemy
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
+    cur.execute("SELECT * \
             FROM cities \
             INNER JOIN states \
             ON cities.state_id = states.id \
