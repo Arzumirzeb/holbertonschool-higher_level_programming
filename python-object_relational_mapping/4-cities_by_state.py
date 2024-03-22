248544acfc5ecdfbd02baf831093db73d66f2b7e
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 22:31:24 +0400

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+import sqlalchemy
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            INNER JOIN states \
+            ORDER BY cities.id")
+    [print(state) for state in cur.fetchall()]
