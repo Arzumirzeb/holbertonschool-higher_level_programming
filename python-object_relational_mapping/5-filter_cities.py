47f0a9119f671c5d48fd8a76f1023fe0246e8801
arzumirzeb <arzumirzeb03@gmail.com>
2024-03-22 23:15:15 +0400

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+import sqlalchemy
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            INNER JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id")
+    print(", ".join([c[2] for c in cur.fetchall() if c[4] == sys.argv[4]]))
